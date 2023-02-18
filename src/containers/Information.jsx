287d4899eabab494201a5a0c5261c63a4f005269
Alexander <ablancoa95@gmail.com>
2023-02-18 15:52:38 -0300

    [UPDATE] maquetacion de la pagina de informacion

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/Information.css'
 
 export default function Information() {
   return (
-    <h1>Information</h1>
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto</h2>
+        </div>
+        <div className="Information-form">
+          <form action=''>
+            <input type='text' placeholder='Nombre completo' name='name' />
+            <input type='email' placeholder='Correo electronico' name='email' />
+            <input type='text' placeholder='Direccion' name='address' />
+            <input type='text' placeholder='Apto' name='apto' />
+            <input type='text' placeholder='Ciudad' name='city' />
+            <input type='text' placeholder='Pais' name='country' />
+            <input type='text' placeholder='Estado' name='state' />
+            <input type='text' placeholder='Codigo postal' name='cp' />
+            <input type='text' placeholder='Telefono' name='phone' />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">
+            Regresar
+          </div>
+          <div className="Information-next">
+            Avanzar al pago
+          </div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedidos</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
