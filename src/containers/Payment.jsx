70bc23e2f29428918aba6142d99fb29dc0c29acf
Alexander <ablancoa95@gmail.com>
2023-02-22 18:42:26 -0300

    [ADD] api de mapa de google maps

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import AppContext from '../context/AppContext';
 import Paypal from '../components/Paypal';
+import Maps from '../components/Maps';
 import '../styles/components/Payment.css'
 
 export default function Payment() {
+
   const { state } = useContext(AppContext);
   const { cart, buyer } = state;
+  const address = useGoogleAddress(buyer[0].address, buyer[0].city)
 
   let total = 0;
-  cart.length > 0 ? cart.map((item) => {
+  (cart && cart.length > 0) ? cart.map((item) => {
     let quantity = item.quantity;
     let price = item.price;
     total = total + (quantity * price)
@@ -16,28 +20,39 @@ export default function Payment() {
   }) : 0
 
   return (
-    <div className="Payment">
-      <div className="Payment-content">
-        <h3>Datos del comprador:</h3>
-        <div className="Buyer-data">
-          <p>{`Name: ${buyer[0].name}`}</p>
-          <p>{`Email: ${buyer[0].email}`}</p>
-          <p>{`Address: ${buyer[0].address} ${buyer[0].apto} ${buyer[0].city} ${buyer[0].state} ${buyer[0].country}`}</p>
-          <p>{`Phone: ${buyer[0].phone}`}</p>
+    <>
+      <div className="Payment">
+        <div className="Payment-content">
+          <h3>Datos del comprador:</h3>
+          {buyer ?
+            (
+              <div className="Buyer-data">
+                <p>{`Name: ${buyer[0].name}`}</p>
+                <p>{`Email: ${buyer[0].email}`}</p>
+                <p>{`Address: ${buyer[0].address} ${buyer[0].apto} ${buyer[0].city} ${buyer[0].state} ${buyer[0].country}`}</p>
+                <p>{`Phone: ${buyer[0].phone}`}</p>
+              </div>
+            ) : ""}
         </div>
-        <h3>Resumen del pedido:</h3>
-        {cart.map((item) => (
-          <div className="Payment-item" key={item.title}>
-            <div className="Payment-element">
-              <h4>{item.title} X {item.quantity}</h4>
-              <span>{item.price}</span>
+        <div>
+
+          <div className='Payment-sideBar'>
+            <h3>Resumen del pedido:</h3>
+            {cart.map((item) => (
+              <div className="Payment-item" key={item.title}>
+                <div className="Payment-element">
+                  <h4>{item.title} X {item.quantity}</h4>
+                  <span>{item.price}</span>
+                </div>
+              </div>
+            ))}
+            <div className="Payment-button">
+              <Paypal sumTotal={total} cart={cart} buyer={buyer[0]} />
             </div>
           </div>
-        ))}
-        <div className="Payment-button">
-          <Paypal sumTotal={total} cart={cart} buyer={buyer[0]} />
         </div>
       </div>
-    </div>
+      {address && <Maps location={address} />}
+    </>
   )
 }
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,25 +1,27 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
+
 import '../styles/components/Success.css'
 
 export default function Success() {
   const { state } = useContext(AppContext)
   const { cart, buyer } = state;
+
   return (
     <div className="Success">
       <div className="Success-content">
         <h3>Datos del comprador:</h3>
-        <div className="Buyer-data">
-          <p>{`Name: ${buyer[0].name}`}</p>
-          <p>{`Email: ${buyer[0].email}`}</p>
-          <p>{`Address: ${buyer[0].address} ${buyer[0].apto} ${buyer[0].city} ${buyer[0].state} ${buyer[0].country}`}</p>
-          <p>{`Phone: ${buyer[0].phone}`}</p>
-        </div>
+        {buyer ?
+          (
+            <div className="Buyer-data">
+              <p>{`Name: ${buyer[0].name}`}</p>
+              <p>{`Email: ${buyer[0].email}`}</p>
+              <p>{`Address: ${buyer[0].address} ${buyer[0].apto} ${buyer[0].city} ${buyer[0].state} ${buyer[0].country}`}</p>
+              <p>{`Phone: ${buyer[0].phone}`}</p>
+            </div>
+          ) : ""}
         <h2>Gracias por tu compra</h2>
         <span>Tu pedido llegara en tres dias</span>
-        <div className="Success-map">
-          Googe Maps
-        </div>
       </div>
     </div>
   )
