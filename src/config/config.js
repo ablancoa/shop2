f1e33265178796f5afce0387a533a04674717ed8
Alexander <ablancoa95@gmail.com>
2023-02-22 22:56:46 -0300

    [UPDATE] variables secrets

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = process.env.REACT_APP_PAYPAL_CLIENT_ID;
-export const GOOGLE_MAPS_KEY = process.env.REACT_APP_GOOGLE_MAPS_KEY
-export const APP_SECRET = "<EGJHVKL02r2Z3HLoKksiDKRF4kQ0PD_pF1srmkQtOv-ulgYcAY10xprtvTFu_FZ-dd9a8ptpTndDxQJ->"
+export const GOOGLE_MAPS_KEY = process.env.REACT_APP_GOOGLE_MAPS_KEY;
+export const APP_SECRET = process.env.REACT_APP_PAYPAL_APP_SECRET;
